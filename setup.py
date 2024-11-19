39225ef3f99acd5c0ce39952560999be502976b9
josephchinedu <josephchinedu34@gmail.com>
2024-11-19 22:30:27 +0100

    made modification on deleting individual app migrations

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-migration-cleaner",
-    version="1.0.0",  
+    version="1.0.2",  
     author="Devjoseph",
     author_email="joseph4jubilant@gmail.com",
     description="A Django management command to delete migrations.",
